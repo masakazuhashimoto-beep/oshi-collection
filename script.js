1b17856b4282b2c4b1a0b1fe49b4d41d2fcb81e2
masakazuhashimoto-beep <masakazu.hashimoto@widsley.com>
2026-08-21 20:02:28 +0900

    S1-S4: 推しコレクション フロントエンド完成

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict";
+console.log('動いた。');
+const title = document.querySelector('#site-title');
+if (title) {
+    title.textContent = 'テーマはなんでもOK！';
+}
+if (title) {
+    title.style.color = 'blue';
+}
+// ボタンとメッセージをhtmlから取得
+const btn = document.querySelector('.submit-btn');
+const message = document.querySelector('#form-message');
+if (btn && message) {
+    // クリックしたらテキストが変わる
+    btn.addEventListener('click', () => {
+        message.textContent = 'クリックしたよ！';
+    });
+    // マウスオーバーしたらテキストが変わる
+    btn.addEventListener('mouseover', () => {
+        message.textContent = 'マウスオーバーしたよ！';
+    });
+    // マウスアウトしたらテキストが変わる
+    btn.addEventListener('mouseout', () => {
+        message.textContent = 'マウスアウトしたよ！';
+    });
+}
+// いいねボタンをクリックしたらカウントが増える
+let count = 0;
+// いいねボタンとカウントをhtmlから取得
+const likeBtn = document.getElementById('like-btn');
+const likeCount = document.getElementById('like-count');
+console.log('likeBtn:', likeBtn);
+console.log('likeCount:', likeCount);
+if (likeBtn && likeCount) {
+    likeBtn.addEventListener('click', () => {
+        count++;
+        likeCount.textContent = String(count);
+        console.log('カウント:', count);
+        // カウントが10になったらメッセージが表示される
+        if (count === 10) {
+            likeCount.textContent = '10いいね達成！ありがとうございます！';
+        }
+    });
+}
+else {
+    console.error('いいね用の要素が見つかりません');
+}
+const items = [
+    { title: 'テスト1', category: 'カテゴリ1', description: '説明1', quantity: 2 },
+    { title: 'テスト2', category: 'カテゴリ2', description: '説明2', quantity: 3 },
+    { title: 'テスト3', category: 'カテゴリ3', description: '説明3', quantity: 5 },
+];
